5a2ea844c36c1319e27c44e49c9ed979378abb27
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 12:19:58 +0530

    Update and rename rhel7_3.5.3.1.2.sh to rhel8_3.5.3.1.2.sh

--- rhel7_3.5.3.1.2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# 3.5.3.1.2 Ensure nftables is not installed with iptables
-echo
-echo \*\*\*\* 3.5.3.1.2  Ensure nftables is not installed with iptables
-rpm -q nftables && yum remove nftables
-
-
-
---
+++ rhel8_3.5.3.1.2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# 3.5.3.1.2 Ensure nftables is not installed with iptables
+#echo
+#cho \*\*\*\* 3.5.3.1.2  Ensure nftables is not installed with iptables
+#rpm -q nftables && yum remove nftables
+
+
+
