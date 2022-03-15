40503f9730153f356c57b1652339224852397dd3
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 11:33:10 +0530

    Delete rhel7_1.1.6.sh

--- rhel7_1.1.6.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# 1.1.6 Ensure /dev/shm is configured
-echo
-echo \*\*\*\* 1.1.6 Ensure /dev/shm is configured
-grep -E '\s/dev/shm\s' /etc/fstab || (echo "tmpfs /dev/shm tmpfs defaults,noexec,nodev,nosuid,seclabel 0 0" >>/etc/fstab && mount -o remount,noexec,nodev,nosuid /dev/shm)
