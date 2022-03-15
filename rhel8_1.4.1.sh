2337d05f39331f36f5211f83b04d4eb4fba57a5f
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 11:39:39 +0530

    Update and rename rhel7_1.3.1.sh to rhel8_1.4.1.sh

--- rhel8_1.4.1.sh
+++ rhel8_1.4.1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# 1.3.1 Ensure AIDE is installed
+# 1.4.1 Ensure AIDE is installed
 echo
-echo \*\*\*\* 1.3.1  Ensure AIDE is installed
+echo \*\*\*\* 1.4.1  Ensure AIDE is installed
 rm -f /etc/yum.repos.d/redhat.repo
 rpm -q aide || yum -y install aide
 
