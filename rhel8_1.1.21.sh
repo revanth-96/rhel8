c7ed3f6de60219659c62a1f8ae3fee3780f239f0
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 11:35:20 +0530

    Update and rename rhel7_1.1.22.sh to rhel8_1.1.21.sh

--- rhel8_1.1.21.sh
+++ rhel8_1.1.21.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# 1.1.22 Ensure sticky bit is set on all world-writable directories
+# 1.1.21 Ensure sticky bit is set on all world-writable directories
 echo
-echo \*\*\*\* 1.1.22 Ensure sticky bit is set on all world-writable directories
+echo \*\*\*\* 1.1.21 Ensure sticky bit is set on all world-writable directories
 df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -type d \( -perm -0002 -a ! -perm -1000 \) 2>/dev/null | xargs chmod a+t
 
