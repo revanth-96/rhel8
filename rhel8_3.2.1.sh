4d5e7dbecd854fc60cea379ecd5a880c86e67186
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 12:11:53 +0530

    Update and rename rhel7_3.3.1.sh to rhel8_3.2.1.sh

--- rhel8_3.2.1.sh
+++ rhel8_3.2.1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-# 3.3.1 Ensure source routed packets are not accepted
+# 3.2.1 Ensure source routed packets are not accepted
 echo
-echo \*\*\*\* 3.3.1 Ensure source routed packets are not accepted
+echo \*\*\*\* 3.2.1 Ensure source routed packets are not accepted
 grep "net.ipv4.conf.all.accept_source_route" /etc/sysctl.conf && sed -ri "s/^(\s*)net.ipv4.conf.all.accept_source_route\s*=\s*\S+(\s*#.*)?\s*$/\1net.ipv4.conf.all.accept_source_route = 0\2/" /etc/sysctl.conf || echo "net.ipv4.conf.all.accept_source_route = 0" >> /etc/sysctl.conf
 grep "net.ipv4.conf.default.accept_source_route" /etc/sysctl.conf && sed -ri "s/^(\s*)net.ipv4.conf.default.accept_source_route\s*=\s*\S+(\s*#.*)?\s*$/\1net.ipv4.conf.default.accept_source_route = 0\2/" /etc/sysctl.conf || echo "net.ipv4.conf.default.accept_source_route = 0" >> /etc/sysctl.conf
 grep "net.ipv6.conf.all.accept_source_route" /etc/sysctl.conf && sed -ri "s/^(\s*)net.ipv6.conf.all.accept_source_route\s*=\s*\S+(\s*#.*)?\s*$/\1net.ipv6.conf.all.accept_source_route = 0\2/" /etc/sysctl.conf || echo "net.ipv6.conf.all.accept_source_route = 0" >> /etc/sysctl.conf
