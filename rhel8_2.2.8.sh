7b73e6f56d31b85de114f5cc0e9f91f6eceb5550
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 11:58:08 +0530

    Update and rename rhel7_2.2.10.sh to rhel8_2.2.8.sh

---
+++ rhel8_2.2.8.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# 2.2.8 Ensure IMAP and POP3 server is not enabled
+echo
+echo \*\*\*\* 2.2.8 Ensure IMAP and POP3 server is not enabled
+systemctl disable dovecot.service
+
+
+
+
+
+
