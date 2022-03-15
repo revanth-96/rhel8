e4c1483313f0c5dbac9f786e5755a41314b680b6
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 12:40:59 +0530

    Update and rename rhel7_5.3.10.sh to rhel8_5.2.10.sh

--- rhel8_5.2.10.sh
+++ rhel8_5.2.10.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# 5.3.10 Ensure SSH root login is disabled 
+# 5.2.10 Ensure SSH root login is disabled 
 echo
-echo \*\*\*\* 5.3.10 Ensure\ SSH\ root\ login\ is\ disabled
+echo \*\*\*\* 5.2.10 Ensure\ SSH\ root\ login\ is\ disabled
 grep "PermitRootLogin" /etc/ssh/sshd_config && sed -i 's/.*PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config || echo "PermitRootLogin no" >> /etc/ssh/sshd_config
 systemctl restart sshd
 
