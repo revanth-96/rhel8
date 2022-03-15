660319565a7fd8dc6eaddcf67988fa08924610be
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 11:28:15 +0530

    Update and rename rhel7_1.1.1.3.sh to rhel8_1.1.1.4.sh

--- rhel7_1.1.1.3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# 1.1.1.3 Ensure mounting of udf filesystems is disabled
-echo
-echo \*\*\*\* 1.1.1.3 Ensure mounting of udf filesystems is disabled
-modprobe -n -v udf | grep "install /bin/true" || echo "install udf /bin/true" >> /etc/modprobe.d/CIS.conf
-lsmod | egrep "udf" && rmmod udf
