a400301fab394d1dd2e726901808780df06dc65e
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 12:20:56 +0530

    Update and rename rhel7_3.5.3.1.3.sh to rhel8_3.5.3.1.3.sh

--- rhel7_3.5.3.1.3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# 3.5.3.1.3 Ensure firewalld is either not installed or masked with iptables
-echo
-echo \*\*\*\* 3.5.3.1.3  Ensure firewalld is either not installed or masked with iptables
-yum -y remove firewalld
-
-
-
