d69701e03a5374e04e0fbecd9b79505fcc3760f3
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 12:41:53 +0530

    Update and rename rhel7_5.3.12.sh to rhel8_5.2.12.sh

--- rhel8_5.2.12.sh
+++ rhel8_5.2.12.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# 5.3.12 Ensure SSH PermitUserEnvironment is disabled
+# 5.2.12 Ensure SSH PermitUserEnvironment is disabled
 echo
-echo \*\*\*\* 5.3.12 Ensure SSH PermitUserEnvironment is disabled
+echo \*\*\*\* 5.2.12 Ensure SSH PermitUserEnvironment is disabled
 egrep -q "^(\s*)PermitUserEnvironment\s+\S+(\s*#.*)?\s*$" /etc/ssh/sshd_config && sed -ri "s/^(\s*)PermitUserEnvironment\s+\S+(\s*#.*)?\s*$/\1PermitUserEnvironment no\2/" /etc/ssh/sshd_config
 echo "PermitUserEnvironment no" >> /etc/ssh/sshd_config
 
