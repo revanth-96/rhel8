5012b17324bb4590a3899af17e4c21cc918c5e4d
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 12:10:52 +0530

    Update and rename rhel7_3.2.1.sh to rhel8_3.1.1.sh

--- rhel8_3.1.1.sh
+++ rhel8_3.1.1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# 3.2.1 Ensure IP forwarding is disabled
+# 3.1.1 Ensure IP forwarding is disabled
 echo
-echo \*\*\*\* 3.2.1  Ensure IP forwarding is disabled
+echo \*\*\*\* 3.1.1  Ensure IP forwarding is disabled
 egrep -q "^(\s*)net.ipv4.ip_forward\s*=\s*\S+(\s*#.*)?\s*$" /etc/sysctl.conf && sed -ri "s/^(\s*)net.ipv4.ip_forward\s*=\s*\S+(\s*#.*)?\s*$/\1net.ipv4.ip_forward = 0\2/" /etc/sysctl.conf || echo "net.ipv4.ip_forward = 0" >> /etc/sysctl.conf
 
 
