324179cefd664f19d82fdea51f9300de0eb7e38c
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 12:46:33 +0530

    Update and rename rhel7_5.3.21.sh to rhel8_5.2.18.sh

--- rhel8_5.2.18.sh
+++ rhel8_5.2.18.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# 5.3.21 Ensure SSH MaxStartups is configured
+# 5.2.18 Ensure SSH MaxStartups is configured
 echo
-echo \*\*\*\* 5.3.21 Ensure SSH MaxStartups is configured
+echo \*\*\*\* 5.2.18 Ensure SSH MaxStartups is configured
 sed -i "/^#MaxStartups 10:30:100/ cMaxStartups 10:30:60" /etc/ssh/sshd_config
 sed -i "/^#MaxStartups 10:30:60/ cMaxStartups 10:30:60" /etc/ssh/sshd_config
 
