a42fc3d02f176e7dff03dfc8498d9da7547f61bc
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 12:59:41 +0530

    Update and rename rhel7_6.1.6.sh to rhel8_6.1.7.sh

--- rhel7_6.1.6.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# 6.1.6 Ensure permissions on /etc/gshadow- are configured
-echo
-echo \*\*\*\* 6.1.6 Ensure permissions on /etc/gshadow- are configured
-chmod -t,u-x-s,g-r-w-x-s,o-r-w-x /etc/gshadow-
-
-
-
