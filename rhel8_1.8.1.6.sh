176a1963ee53d1f7f2cfd033594dbe8fb291c555
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 11:49:36 +0530

    Update and rename rhel7_1.7.6.sh to rhel8_1.8.1.6.sh

---
+++ rhel8_1.8.1.6.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 1.8.1.6 Ensure permissions on /etc/issue.net are configured
+echo
+echo \*\*\*\* 1.8.1.6 Ensure permissions on /etc/issue.net are configured
+chmod -t,u+r+w-x-s,g+r-w-x-s,o+r-w-x /etc/issue.net
+
+
+
+
+
