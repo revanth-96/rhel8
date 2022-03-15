1ae021b76a1360693f6953ea762074e56c35b472
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 12:15:59 +0530

    Update and rename rhel7_3.3.8.sh to rhel8_3.2.8.sh

--- rhel8_3.2.8.sh
+++ rhel8_3.2.8.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 3.3.8 Ensure TCP SYN Cookies is enabled
+# 3.2.8 Ensure TCP SYN Cookies is enabled
 echo
-echo \*\*\*\* 3.3.8 Ensure\ TCP\ SYN\ Cookies\ is\ enabled
+echo \*\*\*\* 3.2.8 Ensure\ TCP\ SYN\ Cookies\ is\ enabled
 egrep -q "^(\s*)net.ipv4.tcp_syncookies\s*=\s*\S+(\s*#.*)?\s*$" /etc/sysctl.conf && sed -ri "s/^(\s*)net.ipv4.tcp_syncookies\s*=\s*\S+(\s*#.*)?\s*$/\1net.ipv4.tcp_syncookies = 1\2/" /etc/sysctl.conf || echo "net.ipv4.tcp_syncookies = 1" >> /etc/sysctl.conf
