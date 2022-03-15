15b0ce49e4fbc9c5049e749c5276ac3876f849ce
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 12:48:58 +0530

    Update and rename rhel7_5.3.6.sh to rhel8_5.2.6.sh

--- rhel8_5.2.6.sh
+++ rhel8_5.2.6.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# 5.3.6 Ensure SSH X11 forwarding is disabled
+# 5.2.6 Ensure SSH X11 forwarding is disabled
 echo
-echo \*\*\*\* 5.3.6 Ensure SSH X11 forwarding is disabled
+echo \*\*\*\* 5.2.6 Ensure SSH X11 forwarding is disabled
 egrep -q "^(\s*)X11Forwarding\s+\S+(\s*#.*)?\s*$" /etc/ssh/sshd_config && sed -ri "s/^(\s*)X11Forwarding\s+\S+(\s*#.*)?\s*$/\1X11Forwarding no\2/" /etc/ssh/sshd_config || echo "X11Forwarding no" >> /etc/ssh/sshd_config
 
 
