2a3e774b98a5832dcdf4137e4a1e8e4a1a22acaa
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 11:32:00 +0530

    Update and rename rhel7_1.1.5.sh to rhel8_1.1.4.sh

--- rhel7_1.1.5.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# 1.1.5 Ensure nosuid option set on /tmp partition
-echo
-echo \*\*\*\* 1.1.5 Ensure nosuid option set on /tmp partition
-findmnt -n /tmp | grep -i nosuid || mount -o remount,nosuid,nodev,noexec /tmp
