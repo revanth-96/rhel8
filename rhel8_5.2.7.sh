56f61fb819549bf8068d128572839a259b5941fc
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 12:49:30 +0530

    Update and rename rhel7_5.3.7.sh to rhel8_5.2.7.sh

--- rhel8_5.2.7.sh
+++ rhel8_5.2.7.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# 5.3.7 Ensure SSH MaxAuthTries is set to 4 or less
+# 5.2.7 Ensure SSH MaxAuthTries is set to 4 or less
 echo
-echo \*\*\*\* 5.3.7 Ensure SSH MaxAuthTries is set to 4 or less
+echo \*\*\*\* 5.2.7 Ensure SSH MaxAuthTries is set to 4 or less
 egrep -q "^(\s*)MaxAuthTries\s+\S+(\s*#.*)?\s*$" /etc/ssh/sshd_config && sed -ri "s/^(\s*)MaxAuthTries\s+\S+(\s*#.*)?\s*$/\1MaxAuthTries 4\2/" /etc/ssh/sshd_config || echo "MaxAuthTries 4" >> /etc/ssh/sshd_config
 
 
