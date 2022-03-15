1a6f70feba516cd6f63d4a5e9882c4c58965b153
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 11:46:53 +0530

    Update and rename rhel7_1.7.2.sh to rhel8_1.8.1.2.sh

---
+++ rhel8_1.8.1.2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 1.8.1.2 Ensure local login warning banner is configured properly
+echo
+echo \*\*\*\* 1.8.1.2  Ensure local login warning banner is configured properly
+[ -s /etc/issue ] && echo "All activities performed on this system will be monitored." > /etc/issue
+
+
+
+
+
