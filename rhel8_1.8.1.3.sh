574fd1034938f56f1e9152bc567632df1480e98d
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 11:47:31 +0530

    Update and rename rhel7_1.7.3.sh to rhel8_1.8.1.3.sh

---
+++ rhel8_1.8.1.3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# 1.8.1.3 Ensure remote login warning banner is configured properly
+echo
+echo \*\*\*\* 1.8.1.3 Ensure remote login warning banner is configured properly
+[ -s /etc/issue.net ] && echo "All activities performed on this system will be monitored." > /etc/issue.net
+
+
+
+
+
+
