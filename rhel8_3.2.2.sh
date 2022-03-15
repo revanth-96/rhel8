d70ab8594adf731491b09643263926620b454d66
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 12:12:26 +0530

    Update and rename rhel7_3.3.2.sh to rhel8_3.2.2.sh

--- rhel8_3.2.2.sh
+++ rhel8_3.2.2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-# 3.3.2 Ensure ICMP redirects are not accepted
+# 3.2.2 Ensure ICMP redirects are not accepted
 echo
-echo \*\*\*\* 3.3.2 Ensure ICMP redirects are not accepted
+echo \*\*\*\* 3.2.2 Ensure ICMP redirects are not accepted
 grep "net.ipv4.conf.all.accept_redirects" /etc/sysctl.conf && sed -ri "s/^(\s*)net.ipv4.conf.all.accept_redirects\s*=\s*\S+(\s*#.*)?\s*$/\1net.ipv4.conf.all.accept_redirects = 0\2/" /etc/sysctl.conf || echo "net.ipv4.conf.all.accept_redirects = 0" >> /etc/sysctl.conf
 grep "net.ipv4.conf.default.accept_redirects" /etc/sysctl.conf && sed -ri "s/^(\s*)net.ipv4.conf.default.accept_redirects\s*=\s*\S+(\s*#.*)?\s*$/\1net.ipv4.conf.default.accept_redirects = 0\2/" /etc/sysctl.conf || echo "net.ipv4.conf.default.accept_redirects = 0" >> /etc/sysctl.conf
 grep "net.ipv6.conf.all.accept_redirects" /etc/sysctl.conf && sed -ri "s/^(\s*)net.ipv6.conf.all.accept_redirects\s*=\s*\S+(\s*#.*)?\s*$/\1net.ipv6.conf.all.accept_redirects = 0\2/" /etc/sysctl.conf || echo "net.ipv6.conf.all.accept_redirects = 0" >> /etc/sysctl.conf
