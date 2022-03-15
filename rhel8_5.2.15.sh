ab4a83d138a7d15704081d365fca8ece36ac43c0
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 12:45:58 +0530

    Update and rename rhel7_5.3.18.sh to rhel8_5.2.15.sh

--- rhel8_5.2.15.sh
+++ rhel8_5.2.15.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# 5.3.18 Ensure SSH warning banner is configured
+# 5.2.15 Ensure SSH warning banner is configured
 echo
-echo \*\*\*\* 5.3.18 Ensure SSH warning banner is configured
+echo \*\*\*\* 5.2.15 Ensure SSH warning banner is configured
 egrep -q "^(\s*)Banner\s+\S+(\s*#.*)?\s*$" /etc/ssh/sshd_config && sed -ri "s/^(\s*)Banner\s+\S+(\s*#.*)?\s*$/\1Banner \/etc\/issue.net\2/" /etc/ssh/sshd_config || echo "Banner /etc/issue.net" >> /etc/ssh/sshd_config
 
 
