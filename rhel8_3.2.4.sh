7845866786afae0902e843de51dc92f95537f09b
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 12:13:27 +0530

    Update and rename rhel7_3.3.4.sh to rhel8_3.2.4.sh

--- rhel8_3.2.4.sh
+++ rhel8_3.2.4.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# 3.3.4 Ensure suspicious packets are logged
+# 3.2.4 Ensure suspicious packets are logged
 echo
-echo \*\*\*\* 3.3.4 Ensure suspicious packets are logged
+echo \*\*\*\* 3.2.4 Ensure suspicious packets are logged
 grep "net.ipv4.conf.all.log_martians" /etc/sysctl.conf && sed -ri "s/^(\s*)net.ipv4.conf.all.log_martians\s*=\s*\S+(\s*#.*)?\s*$/\1net.ipv4.conf.all.log_martians = 1\2/" /etc/sysctl.conf || echo "net.ipv4.conf.all.log_martians = 1" >> /etc/sysctl.conf
 grep "net.ipv4.conf.default.log_martians" /etc/sysctl.conf && sed -ri "s/^(\s*)net.ipv4.conf.default.log_martians\s*=\s*\S+(\s*#.*)?\s*$/\1net.ipv4.conf.default.log_martians = 1\2/" /etc/sysctl.conf || echo "net.ipv4.conf.default.log_martians = 1" >> /etc/sysctl.conf
 sysctl -w net.ipv4.conf.all.log_martians=1
