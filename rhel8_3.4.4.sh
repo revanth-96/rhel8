71e597bbce9cc9dd76d16d504c0fa5a24484e670
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 12:28:37 +0530

    Update rhel8_3.4.4.sh

--- rhel8_3.4.4.sh
+++ rhel8_3.4.4.sh
@@ -1,9 +1,41 @@
-#!/bin/bash
+##!/bin/bash
 
-# 3.4.4 Ensure iptables packages are installed
+## 3.4.4 Configure IPv4 iptables
 #echo
-#echo \*\*\*\* 3.4.4  Ensure iptables packages are installed
-#rpm -q iptables || yum -y install iptables
-#rpm -q iptables-services || yum -y install iptables-services
+#echo \*\*\*\* 3.4.4 Configure IPv4 iptables
+
+## 3.4.4.1.2 Ensure iptables loopback traffic is configured
+#sudo iptables -A INPUT -i lo -j ACCEPT
+#sudo iptables -A OUTPUT -o lo -j ACCEPT
+#sudo iptables -A INPUT -s 127.0.0.0/8 -j DROP
+
+## 3.4.4.1.3 Ensure iptables outbound and established connections are
+#sudo iptables -A OUTPUT -p tcp -m state --state NEW,ESTABLISHED -j ACCEPT
+#udo iptables -A OUTPUT -p udp -m state --state NEW,ESTABLISHED -j ACCEPT
+#sudo iptables -A OUTPUT -p icmp -m state --state NEW,ESTABLISHED -j ACCEPT
+#sudo iptables -A INPUT -p tcp -m state --state ESTABLISHED -j ACCEPT
+#sudo iptables -A INPUT -p udp -m state --state ESTABLISHED -j ACCEPT
+#sudo iptables -A INPUT -p icmp -m state --state ESTABLISHED -j ACCEPT
+
+## 3.4.4.1.4 Ensure iptables rules exist for all open ports
+#sudo iptables -A INPUT -p tcp --dport 22 -m state --state NEW -j ACCEPT
+#sudo iptables -A INPUT -p tcp --dport 443 -m state --state NEW -j ACCEPT
+#sudo iptables -A INPUT -p tcp --dport 80 -m state --state NEW -j ACCEPT
+#sudo iptables -A INPUT -p tcp --dport 514 -m state --state NEW -j ACCEPT
+
+## 3.4.4.1.1 Ensure iptables default deny firewall policy
+#sudo iptables -P INPUT DROP
+#sudo iptables -P OUTPUT DROP
+#sudo iptables -P FORWARD DROP
+
+## 3.5.3.2.5 Ensure iptables rules are saved
+#service iptables save
+
+## 3.4.4.1.5 Ensure iptables is enabled and running
+#systemctl --now enable iptables
+
+
+
+
 
 
