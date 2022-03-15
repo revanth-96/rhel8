92e92c4da28cdc04c9170c7a6e08092fb0f6a589
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 11:36:16 +0530

    Update and rename rhel7_1.1.24.sh to rhel8_1.1.23.sh

--- rhel8_1.1.23.sh
+++ rhel8_1.1.23.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# 1.1.24 Disable USB Storage
+# 1.1.23 Disable USB Storage
 echo
-echo \*\*\*\* 1.1.24 Disable USB Storage
+echo \*\*\*\* 1.1.23 Disable USB Storage
 modprobe -n -v usb-storage | grep "install /bin/true" || echo "install usb-storage /bin/true" >> /etc/modprobe.d/CIS.conf
 lsmod | egrep "usb-storage" && rmmod usb-storage
 
