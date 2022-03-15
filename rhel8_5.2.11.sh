bfdd17877ec5ff29fd9d2c182b69c8cadad29de4
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 12:41:24 +0530

    Update and rename rhel7_5.3.11.sh to rhel8_5.2.11.sh

--- rhel8_5.2.11.sh
+++ rhel8_5.2.11.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# 5.3.11 Ensure SSH PermitEmptyPasswords is disabled
+# 5.2.11 Ensure SSH PermitEmptyPasswords is disabled
 echo
-echo \*\*\*\* 5.3.11 Ensure SSH PermitEmptyPasswords is disabled
+echo \*\*\*\* 5.2.11 Ensure SSH PermitEmptyPasswords is disabled
 egrep -q "^(\s*)PermitEmptyPasswords\s+\S+(\s*#.*)?\s*$" /etc/ssh/sshd_config && sed -ri "s/^(\s*)PermitEmptyPasswords\s+\S+(\s*#.*)?\s*$/\1PermitEmptyPasswords no\2/" /etc/ssh/sshd_config || echo "PermitEmptyPasswords no" >> /etc/ssh/sshd_config
 
 
