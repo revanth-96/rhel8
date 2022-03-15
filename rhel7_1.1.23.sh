e039f2087a1a8e40cb44357d30b2ec1e71987482
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 11:35:47 +0530

    Update and rename rhel7_1.1.23.sh to rhel8_1.1.22.sh

--- rhel7_1.1.23.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# 1.1.23 Disable Automounting
-echo
-echo \*\*\*\* 1.1.23 Disable Automounting
-systemctl disable autofs.service
-
---
+++ rhel8_1.1.22.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# 1.1.22 Disable Automounting
+echo
+echo \*\*\*\* 1.1.22 Disable Automounting
+systemctl disable autofs.service
+
