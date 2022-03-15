6ca1ab04ad8d97a49dbf6a9ff4a0cc6360c0ca03
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 11:46:07 +0530

    Update and rename rhel7_1.7.1.sh to rhel8_1.8.1.1.sh

--- rhel8_1.8.1.1.sh
+++ rhel8_1.8.1.1.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# 1.7.1 Ensure message of the day is configured properly
+# 1.8.1.1 Ensure message of the day is configured properly
 echo
 touch /etc/motd
 chown root:root /etc/motd
 chmod 644 /etc/motd
-echo \*\*\*\* 1.7.1 Ensure message of the day is configured properly
+echo \*\*\*\* 1.8.1.1 Ensure message of the day is configured properly
 [ -s /etc/motd ] || echo "All activities performed on this system will be monitored." > /etc/motd
 sed -ri 's/(\\v|\\r|\\m|\\s)//g' /etc/motd
 
