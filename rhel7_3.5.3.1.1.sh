7102030cc4eecadb8a7642e7c6b6b50b7dd1c444
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 12:18:54 +0530

    Update and rename rhel7_3.5.3.1.1.sh to rhel8_3.4.4.sh

--- rhel7_3.5.3.1.1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# 3.5.3.1.1 Ensure iptables packages are installed
-echo
-echo \*\*\*\* 3.5.3.1.1  Ensure iptables packages are installed
-rpm -q iptables || yum -y install iptables
-rpm -q iptables-services || yum -y install iptables-services
-
-
---
+++ rhel8_3.4.4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# 3.4.4 Ensure iptables packages are installed
+#echo
+#echo \*\*\*\* 3.4.4  Ensure iptables packages are installed
+#rpm -q iptables || yum -y install iptables
+#rpm -q iptables-services || yum -y install iptables-services
+
+
