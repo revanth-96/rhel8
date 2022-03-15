a67013b9ac9c65a8e84e9b9dbbb638bbb1a2ed5f
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 12:53:09 +0530

    Update and rename rhel7_5.4.4.sh to rhel8_5.4.3.sh

--- rhel8_5.4.3.sh
+++ rhel8_5.4.3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#5.4.4 Ensure password reuse is limited
+#5.4.3 Ensure password reuse is limited
 echo
-echo \*\*\*\* 5.4.4 Ensure password reuse is limited
+echo \*\*\*\* 5.4.3 Ensure password reuse is limited
 egrep -q "^\s*password\s+sufficient\s+pam_unix.so(\s+.*)$" /etc/pam.d/system-auth && sed -ri '/^\s*password\s+sufficient\s+pam_unix.so\s+/ { /^\s*password\s+sufficient\s+pam_unix.so(\s+\S+)*(\s+remember=[0-9]+)(\s+.*)?$/! s/^(\s*password\s+sufficient\s+pam_unix.so\s+)(.*)$/\1remember=5 \2/ }' /etc/pam.d/system-auth && sed -ri 's/(^\s*password\s+sufficient\s+pam_unix.so(\s+\S+)*\s+)remember=[0-9]+(\s+.*)?$/\1remember=5\3/' /etc/pam.d/system-auth || echo Ensure\ password\ reuse\ is\ limited /etc/pam.d/system-auth not configured.
 egrep -q "^\s*password\s+sufficient\s+pam_unix.so(\s+.*)$" /etc/pam.d/password-auth && sed -ri '/^\s*password\s+sufficient\s+pam_unix.so\s+/ { /^\s*password\s+sufficient\s+pam_unix.so(\s+\S+)*(\s+remember=[0-9]+)(\s+.*)?$/! s/^(\s*password\s+sufficient\s+pam_unix.so\s+)(.*)$/\1remember=5 \2/ }' /etc/pam.d/password-auth && sed -ri 's/(^\s*password\s+sufficient\s+pam_unix.so(\s+\S+)*\s+)remember=[0-9]+(\s+.*)?$/\1remember=5\3/' /etc/pam.d/password-auth || echo Ensure\ password\ reuse\ is\ limited - /etc/pam.d/password-auth not configured.
