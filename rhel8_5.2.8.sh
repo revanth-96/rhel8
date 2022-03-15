083b724d26944269eb806b380e88ac6beb11a61d
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 12:50:15 +0530

    Update and rename rhel7_5.3.8.sh to rhel8_5.2.8.sh

--- rhel8_5.2.8.sh
+++ rhel8_5.2.8.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# 5.3.8 Ensure SSH IgnoreRhosts is enabled
+# 5.2.8 Ensure SSH IgnoreRhosts is enabled
 echo
-echo \*\*\*\* 5.3.8 Ensure SSH IgnoreRhosts is enabled
+echo \*\*\*\* 5.2.8 Ensure SSH IgnoreRhosts is enabled
 egrep -q "^(\s*)IgnoreRhosts\s+\S+(\s*#.*)?\s*$" /etc/ssh/sshd_config && sed -ri "s/^(\s*)IgnoreRhosts\s+\S+(\s*#.*)?\s*$/\1IgnoreRhosts yes\2/" /etc/ssh/sshd_config || echo "IgnoreRhosts yes" >> /etc/ssh/sshd_config
 
 
