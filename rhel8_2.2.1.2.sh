a05643433e60caa6eb3bc4515ee0e889246501c2
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 13:43:25 +0530

    Update rhel8_2.2.1.2.sh

--- rhel8_2.2.1.2.sh
+++ rhel8_2.2.1.2.sh
@@ -3,7 +3,7 @@
 # 2.2.1.2 Ensure chrony is configured
 echo
 echo \*\*\*\* 2.2.1.2 Ensure chrony is configured
-grep ^OPTIONS /etc/sysconfig/chronyd && sed -i "/^OPTIONS=""/ cOPTIONS=\"-u chrony\"" /etc/sysconfig/chronyd || echo "OPTIONS="-u chrony"" >> /etc/sysconfig/chronyd
+grep -E "^(server)" /etc/chrony.conf || echo "server 10.0.0.2" >> /etc/chrony.conf
 
 
 
