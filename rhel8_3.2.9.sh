e25359942317e3a8fd73a41ed522464a4fcc7851
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 12:16:33 +0530

    Update and rename rhel7_3.3.9.sh to rhel8_3.2.9.sh

--- rhel8_3.2.9.sh
+++ rhel8_3.2.9.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# 3.3.9 Ensure IPv6 router advertisements are not accepted
+# 3.2.9 Ensure IPv6 router advertisements are not accepted
 echo
-echo \*\*\*\* 3.3.9 Ensure IPv6 router advertisements are not accepted
+echo \*\*\*\* 3.2.9 Ensure IPv6 router advertisements are not accepted
 egrep -q "^(\s*)net.ipv6.conf.all.accept_ra\s*=\s*\S+(\s*#.*)?\s*$" /etc/sysctl.conf && sed -ri "s/^(\s*)net.ipv6.conf.all.accept_ra\s*=\s*\S+(\s*#.*)?\s*$/\1net.ipv6.conf.all.accept_ra = 0\2/" /etc/sysctl.conf || echo "net.ipv6.conf.all.accept_ra = 0" >> /etc/sysctl.conf
 egrep -q "^(\s*)net.ipv6.conf.default.accept_ra\s*=\s*\S+(\s*#.*)?\s*$" /etc/sysctl.conf && sed -ri "s/^(\s*)net.ipv6.conf.default.accept_ra\s*=\s*\S+(\s*#.*)?\s*$/\1net.ipv6.conf.default.accept_ra = 0\2/" /etc/sysctl.conf || echo "net.ipv6.conf.default.accept_ra = 0" >> /etc/sysctl.conf
 sysctl -w net.ipv6.conf.all.accept_ra=0
