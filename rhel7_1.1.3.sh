587962244738d6ba06943d6c63e10fc54134827e
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 11:31:01 +0530

    Update and rename rhel7_1.1.3.sh to rhel8_1.1.5.sh

--- rhel7_1.1.3.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# 1.1.3 Ensure noexec option set on /tmp partition
-echo
-echo \*\*\*\* 1.1.3 Ensure noexec option set on /tmp partition
-findmnt -n /tmp | grep -i noexec || mount -o remount,nosuid,nodev,noexec /tmp
