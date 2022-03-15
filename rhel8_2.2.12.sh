b017dae045edac908f2ecddee24efd35c3cc4154
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 12:06:16 +0530

    Add files via upload

---
+++ rhel8_2.2.12.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 2.2.12 Ensure NFS is not enabled
+echo
+echo \*\*\*\* 2.2.12 Ensure NFS is not enabled
+systemctl --now disable nfs-server
+
+
+
+
+
+
+
---
+++ rhel8_2.2.13.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 2.2.13 Ensure RPC is not enabled
+echo
+echo \*\*\*\* 2.2.13 Ensure RPC is not enabled
+systemctl --now disable rpcbind
+
+
+
+
+
+
+
