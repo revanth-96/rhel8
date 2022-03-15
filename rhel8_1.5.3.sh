d9698451aab72b204c68c9b43b8c0e20301496d9
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 11:42:10 +0530

    Update and rename rhel7_1.4.3.sh to rhel8_1.5.3.sh

--- rhel8_1.5.3.sh
+++ rhel8_1.5.3.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# 1.4.3 Ensure authentication required for single user mode
+# 1.5.3 Ensure authentication required for single user mode
 #echo
-#echo \*\*\*\* 1.4.3  Ensure authentication required for single user mode
+#echo \*\*\*\* 1.5.3  Ensure authentication required for single user mode
 #egrep -q "^\s*ExecStart" /usr/lib/systemd/system/rescue.service && sed -ri "s/(^[[:space:]]*ExecStart[[:space:]]*=[[:space:]]*).*$/\1-\/bin\/sh -c \"\/sbin\/sulogin; \/usr\/bin\/systemctl --fail --no-block default\"/" /usr/lib/systemd/system/rescue.service || echo "ExecStart=-/bin/sh -c \"/sbin/sulogin; /usr/bin/systemctl --fail --no-block default\"" >> /usr/lib/systemd/system/rescue.service
 #egrep -q "^\s*ExecStart" /usr/lib/systemd/system/emergency.service && sed -ri "s/(^[[:space:]]*ExecStart[[:space:]]*=[[:space:]]*).*$/\1-\/bin\/sh -c \"\/sbin\/sulogin; \/usr\/bin\/systemctl --fail --no-block default\"/" /usr/lib/systemd/system/emergency.service || echo "ExecStart=-/bin/sh -c \"/sbin/sulogin; /usr/bin/systemctl --fail --no-block default\"" >> /usr/lib/systemd/system/emergency.service
 
