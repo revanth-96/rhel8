5448db1dccef055b07406fc4e416b57f28cb1277
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 11:27:45 +0530

    Update and rename rhel7_1.1.1.2.sh to rhel8_1.1.1.3.sh

--- rhel7_1.1.1.2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# 1.1.1.2  Ensure mounting of squashfs filesystems is disabled
-echo
-echo \*\*\*\* 1.1.1.2 Ensure mounting of squashfs filesystems is disabled
-modprobe -n -v squashfs | grep "install /bin/true" || echo "install squashfs /bin/true" >> /etc/modprobe.d/CIS.conf
-lsmod | egrep "squashfs" && rmmod squashfs
---
+++ rhel8_1.1.1.3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# 1.1.1.3  Ensure mounting of squashfs filesystems is disabled
+echo
+echo \*\*\*\* 1.1.1.3 Ensure mounting of squashfs filesystems is disabled
+modprobe -n -v squashfs | grep "install /bin/true" || echo "install squashfs /bin/true" >> /etc/modprobe.d/CIS.conf
+lsmod | egrep "squashfs" && rmmod squashfs
