5a3d3f6bd9f26ee28213d91dd33b3374db7fa23e
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 12:17:14 +0530

    Update and rename rhel7_3.4.1.sh to rhel8_3.3.1.sh

--- rhel8_3.3.1.sh
+++ rhel8_3.3.1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# 3.4.1 Ensure DCCP is disabled
+# 3.3.1 Ensure DCCP is disabled
 echo
-echo \*\*\*\* 3.4.1 Ensure DCCP is disabled
+echo \*\*\*\* 3.3.1 Ensure DCCP is disabled
 modprobe -n -v dccp | grep "install /bin/true" || echo "install dccp /bin/true" >> /etc/modprobe.d/CIS.conf
 lsmod | egrep "dccp" && rmmod dccp
