b52ec911583907e4277b288dc3cd1f8dcd356345
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 12:44:50 +0530

    Update and rename rhel8_5.3.13.sh to rhel8_5.2.13.sh

--- rhel8_5.2.13.sh
+++ rhel8_5.2.13.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# 5.3.13 Ensure SSH Idle Timeout Interval is configured
+# 5.2.13 Ensure SSH Idle Timeout Interval is configured
 echo
-echo \*\*\*\* 5.3.13 Ensure SSH Idle Timeout Interval is configured
+echo \*\*\*\* 5.2.13 Ensure SSH Idle Timeout Interval is configured
 egrep -q "^(\s*)ClientAliveInterval\s+\S+(\s*#.*)?\s*$" /etc/ssh/sshd_config && sed -ri "s/^(\s*)ClientAliveInterval\s+\S+(\s*#.*)?\s*$/\1ClientAliveInterval 300\2/" /etc/ssh/sshd_config || echo "ClientAliveInterval 300" >> /etc/ssh/sshd_config
 egrep -q "^(\s*)ClientAliveCountMax\s+\S+(\s*#.*)?\s*$" /etc/ssh/sshd_config && sed -ri "s/^(\s*)ClientAliveCountMax\s+\S+(\s*#.*)?\s*$/\1ClientAliveCountMax 3\2/" /etc/ssh/sshd_config || echo "ClientAliveCountMax 3" >> /etc/ssh/sshd_config
 
