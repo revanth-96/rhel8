d6661c4204d2e1ab0a33d80e83e77c4bc51c7495
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 12:56:45 +0530

    Update and rename rhel7_5.5.4.sh to rhel8_5.5.3.sh

--- rhel8_5.5.3.sh
+++ rhel8_5.5.3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# 5.5.4 Ensure default user shell timeout is 900 seconds or less
+# 5.5.3 Ensure default user shell timeout is 900 seconds or less
 echo
-echo \*\*\*\* 5.5.4 Ensure default user shell timeout is 900 seconds or less
+echo \*\*\*\* 5.5.3 Ensure default user shell timeout is 900 seconds or less
 echo -e "TMOUT=900 \nreadonly TMOUT \nexport TMOUT" >> /etc/bash.bashrc
 echo -e "TMOUT=900 \nreadonly TMOUT \nexport TMOUT" >> /etc/profile.d/tmout.sh
