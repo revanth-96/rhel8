d6009a246727a205a0dc58736cfe3b49ade61a8f
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-15 12:17:45 +0530

    Update and rename rhel7_3.4.2.sh to rhel8_3.3.2.sh

--- rhel8_3.3.2.sh
+++ rhel8_3.3.2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# 3.4.2 Ensure SCTP is disabled
+# 3.3.2 Ensure SCTP is disabled
 echo
-echo \*\*\*\* 3.4.2 Ensure SCTP is disabled
+echo \*\*\*\* 3.3.2 Ensure SCTP is disabled
 modprobe -n -v sctp | grep "install /bin/true" || echo "install sctp /bin/true" >> /etc/modprobe.d/CIS.conf
 lsmod | egrep "sctp" && rmmod sctp
 
